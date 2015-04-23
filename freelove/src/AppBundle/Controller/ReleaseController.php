646f390275d89c1d32d488fe2f4b7e372446097d
KeithLongbeard <keithgreenjob@gmail.com>
2015-04-23 17:10:52 +0100

    cms add release form

--- freelove/src/AppBundle/Controller/ReleaseController.php
+++ freelove/src/AppBundle/Controller/ReleaseController.php
@@ -10,13 +10,47 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Release;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ReleaseController extends Controller{
 
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('cms/addRel.html.twig');
+        $release = new Release();
+
+        $form = $this->createFormBuilder($release)
+                    ->add('Artist', 'text')
+                    ->add('Title', 'text')
+                    #->add('Length', 'text') Not needed on account of track times
+                    ->add('Genre', 'text')
+                    ->add('Description', 'textarea')
+                    ->add('Tracks', 'textarea')
+                    ->add('save', 'submit', array('label' => 'Add Release'))
+                    ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isValid()){
+            $tracks = explode(',', $release->getTracks());
+            $bool = false;
+            #FIXME: Needs to work properly, for some reason always gets the last two values without dropping
+            for($i=0; $i<count($tracks)-1; $i++){
+                if(!$bool){
+                    unset($tracks[$i]);
+                    $bool = !$bool;
+                }else{
+                    $bool = !$bool;
+                }
+            }
+
+            var_dump($tracks);
+        }
+
+        return $this->render('cms/addRel.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
     }
 
     public function createAction($artist, $title, $length, $genre, $description, $tracks, $download, $thumb, $art)
@@ -32,16 +66,16 @@ class ReleaseController extends Controller{
         $release->setDownload($download);
         $release->setThumnail($thumb);
         $release->setArtwork($art);
+    }
 
+    public function addToDB(Release $release)
+    {
         $em = $this->getDoctrine()->getManager();
-
         $em->persist($release);
         $em->flush();
-
-
         return new Response('New release added: '.$release->getArtist().' - '.$release->getTitle());
-
     }
+
 
 
 }
