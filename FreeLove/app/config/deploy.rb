2a4a1aa5df6bd7fa2b7b548e47be0555a00eca8f
KeithLongbeard <kgr@bookboon.com>
2016-01-14 22:34:51 +0000

    deployment

---
+++ FreeLove/app/config/deploy.rb
@@ -0,0 +1,24 @@
+set :application, "Free Love"
+set :domain,      "85.25.253.205"
+set :deploy_to,   "httpdocs/freelove"
+set :app_path,    "app"
+set :user,        "lovelove"
+set :use_sudo,    false
+set :scm,         :git
+set :repository,  "file://Users/Keef/Sites/freelove/.git"
+set :deploy_via,  :copy
+set :use_composer, true
+set :update_vendors, true
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :keep_releases,  3
+
+set :deploy_via, :rsync_with_remote_cache
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL
